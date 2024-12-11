307d8f1c489ca2c7edebf06fc2dd54dd7879726d
ShivPatel031 <patelshiv3123@gmail.com>
2024-12-11 12:19:16 +0530

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,130 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();  
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                //ModelState.AddModelError("CustomError", "The displayorder can not exactly match the Name."); go in summary
+                ModelState.AddModelError("Name", "The displayorder can not exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                //return RedirectToAction("Index"(action name),"Home"(controller name)); if want to redirct in other controller
+                TempData["Success"] = "Category created successfully.";
+                return RedirectToAction("Index"); // if redirecting in same controller
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // way to retrive data
+            var obj = _db.Categories.Find(id);
+            //var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var obj = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (obj == null) { return NotFound(); }
+
+            return View(obj);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                //ModelState.AddModelError("CustomError", "The displayorder can not exactly match the Name."); go in summary
+                ModelState.AddModelError("Name", "The displayorder can not exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category updated successfully.";
+                //return RedirectToAction("Index"(action name),"Home"(controller name)); if want to redirct in other controller
+                return RedirectToAction("Index"); // if redirecting in same controller
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // way to retrive data
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null) { return NotFound(); }
+
+            return View(obj);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // way to retrive data
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null) { return NotFound(); }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully.";
+            return RedirectToAction("Index"); 
+        }
+    }
+}
+
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,10,ErrorMessage ="Display Order must be between 1 to 10")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDataTime { get; set; } = DateTime.Now; // to give default date 
+
+    }
+}
